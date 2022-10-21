5ce09af2d1cc2c67b81472a9c9c3ea88ffb7e742
Brandon Woodthorpe <bmwoodthorpe@gmail.com>
2022-10-21 12:39:40 -0400

    Uploaded Files from local

---
+++ app.js
@@ -0,0 +1,14 @@
+const toggleBtn = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link')
+
+
+toggleBtn.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+})
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+})
+
